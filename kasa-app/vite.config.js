336b4d9eaa216cb177f1ef154b036187beefd08a
Jordy Rocacher <jmr4277@orange.fr>
2025-05-20 16:47:31 +0200

    command vite

--- kasa-app/vite.config.js
+++ kasa-app/vite.config.js
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  build: {
+    outDir: "dist", // Doit correspondre au dossier de sortie
+  },
+  preview: {
+    port: 4173, // Port par défaut de vite preview
+    open: true, // Ouvre automatiquement le navigateur
+  },
 });
